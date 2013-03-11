d1712e3ae2552f55c1840a170f867bad34e13569
Bernardo Rittmeyer <bernardorittmeyer@gmail.com>
2013-03-11 17:11:06 +0100

    Form ajax start

--- app/routes.php
+++ app/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function() {
-	$news = News::paginate(20);
+	$news = News::orderby('created_at', 'desc')->paginate(20);
     return View::make('list')->with('news',$news);
 });
 
@@ -51,6 +51,43 @@ Route::post('login', function() {
 });
 
 Route::get('panel', function(){
-    $news = News::paginate(20);
+    $news = News::orderby('created_at', 'desc')->paginate(20);
     return View::make('admin.panel')->with('news',$news);
+});
+
+Route::post('panel', function(){
+
+    $input = array(
+        'name'    => Input::get('name'),
+        'url'     => Input::get('url'),
+        'category'=> Input::get('category')
+    );
+
+    $rule = array(
+        'name'     => 'required', 
+        'url'      => 'required|url',
+        'category' => 'required');
+
+    $validator = Validator::make($input, $rule);
+
+    if ($validator->fails()) {
+        $messages = $validator->messages();
+        return Response::json(array("error" => $messages->first('url')));
+        /*return Redirect::to('panel')->withErrors($validator);*/
+    }
+
+    $new = News::create(array(
+        'name' => $input['name'],
+        'url'  =>  $input['url'],
+        'category' => $input['category'],
+        'user_id' => Auth::user()->id,
+    ));
+
+    $new->date = $new->created_at;
+
+    return Response::json($new->toArray());
+});
+
+Route::any('test', function(){
+    return var_dump(News::find(7)->toJson());
 });
